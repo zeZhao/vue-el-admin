bbc9c26d84433d9bd54a1cb976c8260426816832
zeZhao <zezhao1026@163.com>
2022-06-28 21:28:42 +0800

    配置服务器目录

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,7 @@ const port = process.env.port || process.env.npm_config_port || 8080 // dev port
 
 // All configuration item explanations can be find in https://cli.vuejs.org/config/
 module.exports = {
-  publicPath: '/',
+  publicPath: '/changedata/',
   outputDir: 'dist',
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'dev',
